96b9823f06864d95fa01db8e05f0302ae2759eeb
KaiNguyen <kainguyen.business@gmail.com>
2023-02-04 08:27:57 +0700

    add fiber

--- main.go
+++ main.go
@@ -1,66 +1,39 @@
 package main
 
-import (
-	"context"
-	"github.com/chromedp/chromedp"
-	"github.com/kainguyen/go-scrapper/internal/screenshotter"
-	"log"
-	"os"
-)
-
-func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.WaitVisible(`div#primary`),
-		chromedp.FullScreenshot(res, quality),
-	}
-}
-
-func partlyScreenshot(urlstr string, sel interface{}, buf *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.WaitVisible(sel),
-		chromedp.Screenshot(sel, buf),
-	}
-}
-
-func CaptureDevToWebpage(ctx context.Context, cancelFunc context.CancelFunc) {
-	defer cancelFunc()
-
-	var buf []byte
-
-	// capture entire browser viewport, returning png with quality=90
-	if err := chromedp.Run(ctx, fullScreenshot(`https://dev.to/`, 50, &buf)); err != nil {
-		panic(err)
-	}
-
-	if err := os.WriteFile("devto_thumbnail.jpeg", buf, 0777); err != nil {
-		panic(err)
-	}
-
-	log.Printf("screenshot taken!")
-}
-
-func CaptureYoutubeWebpage(ctx context.Context, cancelFunc context.CancelFunc) {
-	defer cancelFunc()
-
-	var buf []byte
-
-	// capture entire browser viewport, returning png with quality=90
-	if err := chromedp.Run(ctx, partlyScreenshot(`https://www.youtube.com/watch?v=HhanFyk7lTk`, `div#player`, &buf)); err != nil {
-		panic(err)
-	}
-
-	if err := os.WriteFile("youtube_thumbnail.jpeg", buf, 0777); err != nil {
-		panic(err)
-	}
-
-	log.Printf("screenshot taken!")
-}
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	newScreenshotterCtx, cancel := screenshotter.NewScreenshotter()
+	//screenshot.CaptureYoutubeWebpage()
+	//screenshot.CaptureDevToWebpage()
 
-	//CaptureDevToWebpage(newScreenshotterCtx, cancel)
-	CaptureYoutubeWebpage(newScreenshotterCtx, cancel)
+	//c := colly.NewCollector()
+	//
+	//// Before making a request print "Visiting ..."
+	//c.OnRequest(func(r *colly.Request) {
+	//	fmt.Println("Visiting", r.URL.String())
+	//})
+	//
+	//c.OnResponse(func(response *colly.Response) {
+	//	fmt.Println(string(response.Body))
+	//})
+	//
+	//// On every a element which has href attribute call callback
+	////c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	////	link := e.Attr("href")
+	////	// Print link
+	////	fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	////	// Visit link found on page
+	////	// Only those links are visited which are in AllowedDomains
+	////	//c.Visit(e.Request.AbsoluteURL(link))
+	////})
+	//
+	//err := c.Visit("https://hackerspaces.org/")
+	//if err != nil {
+	//	return
+	//}
+
+	var app = fiber.New()
+
+	app.Get("")
+
 }
