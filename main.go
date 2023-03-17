d5bc3a18eda413509e310f2e12199940a784de70
KaiNguyen <kainguyen.business@gmail.com>
2023-03-18 00:49:55 +0700

    test go ast for reading handler file

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/kainguyen/go-scrapper/src/core/application/http"
 	_ "github.com/kainguyen/go-scrapper/src/infrastructure/di"
+	"github.com/kainguyen/go-scrapper/src/utils"
+	"go/parser"
+	"go/token"
 	"log"
 )
+
+func init() {
+	fs := token.NewFileSet()
+
+	fileName := utils.GetWorkDirectory()
+
+	f, err := parser.ParseFile(fs, fmt.Sprintf("%v/src/core/application/http/post/post.handler.go", fileName), nil, parser.AllErrors)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//
+	//fmt.Printf("%#v\n", f)
+
+	spew.Dump(f)
+}
 
 func main() {
 	server, err := http.NewHttpServer()
