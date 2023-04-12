f40d2836288ee0906c2f5921ffb5ef3e47c953c4
KaiNguyen <nguyenduckhai8101@gmail.com>
2023-04-12 12:35:31 +0700

    change client desktop

--- client/desktop/app.go
+++ client/desktop/app.go
@@ -1,32 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-	"image/color"
 )
 
 func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Container")
-	myWindow.Resize(fyne.NewSize(500, 500))
+	a := app.New()
+	w := a.NewWindow("SysTray")
 
-	green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+	//if desk, ok := a.(desktop.App); ok {
+	//	m := fyne.NewMenu("MyApp",
+	//		fyne.NewMenuItem("Show", func() {
+	//			fmt.Println("Showing...")
+	//		}))
+	//
+	//	desk.SetSystemTrayMenu(m)
+	//}
 
-	text := canvas.NewText("Hello World", green)
-	textContainer := container.New(layout.NewCenterLayout(), text)
+	w.Resize(fyne.NewSize(1920, 1080))
 
-	button := widget.NewButton("Click me", func() {
-		fmt.Println("Yes I am clicked!")
-	})
-
-	content := container.New(layout.NewVBoxLayout(), textContainer, button)
-
-	myWindow.SetContent(content)
-	myWindow.ShowAndRun()
+	w.SetContent(widget.NewLabel("Fyne System Tray"))
+	w.ShowAndRun()
 }
